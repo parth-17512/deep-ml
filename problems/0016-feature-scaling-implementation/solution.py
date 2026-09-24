33bc8b7cffd525570fc97354daac7f909ddc4a0c
parth-17512 <206014239+parth-17512@users.noreply.github.com>
2026-09-24 19:46:16 +0000

    Update: Feature Scaling Implementation

--- problems/0016-feature-scaling-implementation/solution.py
+++ problems/0016-feature-scaling-implementation/solution.py
@@ -1,18 +1,19 @@
-import numpy as np
+import  numpy as np
 def feature_scaling(data: np.ndarray) -> (np.ndarray, np.ndarray):
 	# Your code here
 	#standardized_data = (x-mean)/std
-	mean = np.mean(data,axis=0)
+	mean = np.mean(data, axis=0)
 	std = np.std(data, axis=0)
 	standardized_data = (data-mean)/std
 
-	#min-max : (x-min)/(max-min)
+
+	#min -max = (x-min)/(max-min)
 	min_val = np.min(data, axis=0)
-	max_val = np.max(data,axis=0)
+	max_val = np.max(data, axis=0)
 	normalized_data = (data-min_val)/(max_val-min_val)
 
-	#round results to 4 decimal places 
-	standardized_data= np.round(standardized_data, 4)
-	normalized_data= np.round(normalized_data, 4)
-
+	#round the data to 4 decimal
+	standardized_data=np.round(standardized_data, 4)
+	normalized_data = np.round(normalized_data, 4)
+	
 	return standardized_data, normalized_data
